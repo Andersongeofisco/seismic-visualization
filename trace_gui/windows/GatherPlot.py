e6a3d027a3c91a3e1b5586740c104068bf9ffa20
andreabduque <abd@cin.ufpe.br>
2017-05-08 17:06:43 -0300

    wiggle plot

--- trace_gui/windows/GatherPlot.py
+++ trace_gui/windows/GatherPlot.py
@@ -3,6 +3,7 @@ from windows.PlotTraceItem import PlotTraceItem
 from pyqtgraph import PlotCurveItem
 import pyqtgraph as pg
 import numpy as np
+from copy import deepcopy
 
 #Plotar traços do gather
 #Plotar linha vermelha de referência
@@ -41,6 +42,7 @@ class GatherPlotWidget(PlotWidget):
         print(trace.traceParams)
         self.drawRefLine()
 
+    #Wiggle plot
     def plotGather(self):
         traces =  self.gather.scaleTraces(99)
         offsets = self.gather.getOffsets()
@@ -50,14 +52,47 @@ class GatherPlotWidget(PlotWidget):
             self.getPlotItem().clear()
 
         y = np.linspace(0, self.gather.nsamples*self.gather.interval,  self.gather.nsamples)
+        self.setYRange(np.min(y), np.max(y))
+
         for trace, offset, index in zip(traces, offsets, indexes):
-            x = trace + offset
-            tracePlot = PlotTraceItem(x, y, self.gather.getTraceParams(index))
-            tracePlot.sigClicked.connect(self.traceClicked)
-            self.addItem(tracePlot)
+            # print("tamanho")
+            # print(len(trace))
+            # print(len(y))
+            trace[0] = 0
+            trace[-1] = 0
+            #Zero crossings
+            crossing = np.where(np.diff(np.signbit(trace)))[0]
+            #Interpolate
+            x1=  trace[crossing]
+            x2 =  trace[crossing+1]
+            y1 = y[crossing]
+            y2 = y[crossing+1]
+            m = (y2 - y1)/(x2-x1)
+            c = y1 - m*x1
+            xTemp = np.hstack([trace, np.zeros_like(c)])
+            yTemp = np.hstack([y, c])
+            #Resort the data
+            order = np.argsort(yTemp)
+            #New data with zero crossings in correct positions
+            trace = xTemp[order]
+            yTrace = yTemp[order]
+
+            negCurve = deepcopy(trace)
+            posCurve = deepcopy(trace)
+            posCurve[posCurve < 0] = 0
+            negCurve[negCurve > 0] = 0
+
+            posItem = PlotTraceItem(posCurve + offset, yTrace, self.gather.getTraceParams(index))
+            negItem = PlotTraceItem(negCurve + offset, yTrace, self.gather.getTraceParams(index))
+            #Fill only positives of curve
+            posItem.setFillLevel(offset)
+            posItem.setBrush(pg.mkBrush(0,0,0))
+            offItem = PlotCurveItem(np.tile(offset, len(y)), y, pen=pg.mkPen('w', width=1))
+            posItem.sigClicked.connect(self.traceClicked)
+            negItem.sigClicked.connect(self.traceClicked)
+            self.addItem(posItem)
+            self.addItem(negItem)
+            self.addItem(offItem)
 
         if(self.refTrace is None):
-            self.refTrace = tracePlot
-
-
-    #def plotRefTrace():
+            self.refTrace = posItem
